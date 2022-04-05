4cd4da06733f059aeb6032e3011deb35abaa23e5
Stephen Blevins <scblevins27@gmail.com>
2022-04-05 17:34:14 -0400

    styled components

--- frontend/src/components/BugModal.js
+++ frontend/src/components/BugModal.js
@@ -16,24 +16,17 @@ export const BugModal = ({bug}) => {
       <Link to="/edit" state={bug}>
         <FontAwesomeIcon icon={faPencil} className="edit" />
       </Link>
-      <div className="bug-date-wrapper">
-        <label>Date: </label>
-        <div className="bug-date">{time}</div>
-      </div>
       <div className="bug-name-wrapper">
         <h1>{name}</h1>
       </div>
+      <div className="bug-date-wrapper">
+        <div className="bug-date">{time}</div>
+      </div>
       <div className="bug-author-wrapper">
-        <label>Author: </label>
         <div className="bug-author">{author}</div>
       </div>
       <div className="bug-status-wrapper">
-        <label>Status: </label>
-        <div className="bug-status">{status}</div>
-      </div>
-      <div className="bug-desc-wrapper">
-        <label>Description: </label>
-        <div className="bug-desc">{details}</div>
+        <div style={status === "open" ? {backgroundColor: "green"} : {backgroundColor: "red"}} >{status}</div>
       </div>
     </div>
   );
--- frontend/src/components/Bugs.js
+++ frontend/src/components/Bugs.js
@@ -4,6 +4,12 @@ import "../css/bugs.css";
 export const Bugs = ({ data }) => {
   return (
     <div className="bugs-wrapper">
+      <div className="header">
+        <div className="bug-name">Bug</div>
+        <div className="bug-created">Created</div>
+        <div className="bug-creator">Creator</div>
+        <div className="bug-status">Status</div>
+      </div>
       {data[0] !== "nothing" ? (
         data.map((bug) => <BugModal key={bug._id} bug={bug} />)
       ) : (
