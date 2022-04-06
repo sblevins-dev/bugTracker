b9b8b1ebd7f04f63220db9a5ec49d904aa06967e
Stephen Blevins <scblevins27@gmail.com>
2022-04-05 21:26:32 -0400

    added comments to bugModal

--- frontend/src/components/BugModal.js
+++ frontend/src/components/BugModal.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { faPencil, faComment } from "@fortawesome/free-solid-svg-icons";
 
 export const BugModal = ({bug}) => {
-  const { name, author, details, time, priority, status } = bug;
+  const { name, author, details, time, priority, status, comments } = bug;
+  console.log(bug)
 
   useEffect(() => {
 
@@ -18,6 +19,10 @@ export const BugModal = ({bug}) => {
       </Link>
       <div className="bug-name-wrapper">
         <h1>{name}</h1>
+        <div className="comments">
+          <FontAwesomeIcon icon={faComment} className="comment-icon" />
+          <div>{comments.length}</div>
+        </div>
       </div>
       <div className="bug-date-wrapper">
         <div className="bug-date">{time}</div>
