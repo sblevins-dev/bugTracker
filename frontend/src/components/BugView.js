242c052d1b26c7386695f99fac373b19ec874d46
Stephen Blevins <scblevins27@gmail.com>
2022-04-08 22:39:58 -0400

    styled some of the bug view

--- frontend/src/components/BugView.js
+++ frontend/src/components/BugView.js
@@ -1,25 +1,59 @@
-import { useLocation, useNavigate } from "react-router-dom"
-import '../css/bugView.css';
+import { useLocation, useNavigate } from "react-router-dom";
+import "../css/bugView.css";
 
 export const BugView = (props) => {
-    const location = useLocation();
-    const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const {bug} = location.state;
+  const { bug } = location.state;
 
-    const handleClick = () => {
-        navigate('/')
-    }
+  const handleClick = () => {
+    navigate("/");
+  };
   return (
     <div className="bugview-wrapper" onClick={handleClick}>
-      <h1>{bug.name}</h1>
-      <div>{bug.author}</div>
-      <div>{bug.assigned}</div>
-      <div>{bug.status}</div>
-      <p>{bug.details}</p>
-      <div>{bug.steps}</div>
-      <div>{bug.comments}</div>
-      {console.log(bug)}
+      <div className="first-sec">
+        <h1 className="bug-title">{bug.name}</h1>
+        <div className="bug-details">
+          <div className="details-title">Details:</div>
+          <div className="details">{bug.details}</div>
+        </div>
+        <div className="steps-wrapper">
+          <div className="steps-title">Steps Taken</div>
+          <ul className="steps-list">
+            {bug.steps.map((step, i) => (
+              <li key={i}>{step}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="comments-wrapper">
+          <h1>Comments</h1>
+          <ul className="comment-list">
+            {bug.comments.map((comm, i) => (
+              <li key={i}>{comm}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="second-sec">
+        <div className="bug-author">
+          <div className="author-title">Creator</div>
+          <div className="author">{bug.author}</div>
+        </div>
+        <div className="bug-assigned">
+          <div className="assigned-title">Assigned</div>
+          <div className="assigned">{bug.assigned}</div>
+        </div>
+        <div className="bug-status">
+          <div className="status-title">Status</div>
+          <div className="status">{bug.status}</div>
+        </div>
+        <div className="bug-priority">
+          <div className="priority-title">Priority</div>
+          <div className="priority">{bug.status}</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
