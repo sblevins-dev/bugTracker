250c374e801e65b6b21cdb6d5acf52f37e904cd9
Stephen Blevins <scblevins27@gmail.com>
2022-04-24 15:52:00 -0400

    changes to edit page

--- frontend/src/components/Edit.js
+++ frontend/src/components/Edit.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useLocation, Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import "../css/edit.css";
 
 export const Edit = (props) => {
@@ -8,27 +9,52 @@ export const Edit = (props) => {
   const location = useLocation();
   const navigate = useNavigate();
   const bug = location.state;
+  const [step, setStep] = useState("");
 
+  // I added to be able to make everything unified
   const setSteps = () => {
     let obj = {};
-    bug.steps.map((step, i) => {
-      obj[`step ${i + 1}`] = step;
-    });
+    bug.steps.map((step, i) => (obj[`step ${i + 1}`] = step));
 
     return obj;
   };
 
-  const [formData, setFormData] = useState({
+  const initialState = {
     name: bug.name,
     status: bug.status,
     steps: setSteps(),
     details: bug.details,
-  });
+  }
 
+  // set formData
+  const [formData, setFormData] = useState(initialState);
+
+  // necessary right now to map steps and add when necessary
+  const renderSteps = () => {
+    return Object.keys(formData.steps).length > 0 ? (
+      Array.from(
+        Object.entries(formData.steps).map((step, i) => (
+          <input
+            type="text"
+            key={i}
+            value={step[1]}
+            name={step[0]}
+            onChange={setForm}
+            className="step"
+          />
+        ))
+      )
+    ) : (
+      <div style={{ fontSize: "1rem" }}>No Steps Taken</div>
+    );
+  };
+
+  // checked if logged in
   if (!auth.loggedIn) {
     navigate("/");
   }
 
+  // setting form function
   const setForm = (e) => {
     if (e.target.name.split(" ").includes("step")) {
       setFormData({
@@ -46,8 +72,9 @@ export const Edit = (props) => {
     }
   };
 
+  // handle for mark complete or re open ticket
   const handleCheck = (e) => {
-    let isChecked = e.target.checked
+    let isChecked = e.target.checked;
     let open = "open";
     let closed = "closed";
     let tempStatus = bug.status;
@@ -70,11 +97,38 @@ export const Edit = (props) => {
     });
   };
 
+  // add step to form data
+  const addStep = (e) => {
+    e.preventDefault();
+    if (step !== '') {
+      let index = Object.keys(formData.steps).length;
+      let name = `step ${index + 1}`;
+
+      setFormData({
+        ...formData,
+        steps: {
+          ...formData.steps,
+          [name]: step,
+        },
+      });
+
+      setStep("");
+      toast.success("Step added!", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    } else {
+      toast.error("Please enter valid input", {
+        position: toast.POSITION.BOTTOM_RIGHT
+      })
+    }
+  };
+
+  // submit form
   const handleEdit = (e) => {
     e.preventDefault();
 
-    console.log(formData.status)
-    console.log('form sent')
+    console.log(formData.status);
+    console.log("form sent");
   };
 
   return (
@@ -117,20 +171,18 @@ export const Edit = (props) => {
           </div>
         </div>
         <label>Steps Taken:</label>
-        {bug.steps.length > 0 ? (
-          bug.steps.map((step, i) => (
-            <input
-              type="text"
-              key={i}
-              defaultValue={step}
-              name={`step ${i + 1}`}
-              onChange={setForm}
-            />
-          ))
-        ) : (
-          <div style={{ fontSize: "1rem" }}>No Steps Taken</div>
-        )}
-
+        {renderSteps()}
+        <div className="add-step">
+          <input
+            type="text"
+            value={step}
+            onChange={(e) => setStep(e.target.value)}
+            placeholder="Add a step"
+          />
+        </div>
+        <button className="add-step-btn" onClick={addStep}>
+          Add Step
+        </button>
         <label>Details:</label>
         <textarea
           type="text"
